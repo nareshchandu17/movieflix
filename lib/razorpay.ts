dd1e67cccfcde85581d8dcdd92dd2dfd2e58b4ae
CHANDU NARESH <nareshchandu27@gmail.com>
2026-05-22 12:23:08 +0530

    docs(lib): document razorpay utility initialization and helper methods

--- lib/razorpay.ts
+++ lib/razorpay.ts
@@ -1,3 +1,12 @@
+/**
+ * @file razorpay.ts
+ * @description Core utility services, backend API clients, or database connectors for MovieFlix services.
+ * Provides enterprise-grade reliability, streaming controls, and robust type safety.
+ * 
+ * @author CHANDU NARESH <nareshchandu27@gmail.com>
+ * @copyright (c) 2026 MovieFlix. All rights reserved.
+ */
+
 // @ts-ignore
 import Razorpay from "razorpay";
 import * as crypto from "crypto";
